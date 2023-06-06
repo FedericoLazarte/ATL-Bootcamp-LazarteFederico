9033b1a5e0636b6d345d638691c195831d13688c
FedericoLazarte <fedelazarte2015@gmail.com>
2023-06-06 18:42:41 -0300

    subiendo tercer ejercicio

---
+++ clase5/EjercicioTres.java
@@ -0,0 +1,15 @@
+package clase5;
+import java.util.*;
+public class EjercicioTres {
+    public static void main(String[] args) {
+        Double precioOriginal;
+        Integer descuento;
+        Scanner ingresoDato = new Scanner(System.in);
+        System.out.println("Ingrese precio del producto: ");
+        precioOriginal = ingresoDato.nextDouble();
+        System.out.println("Ingrese porcentaje de descuento: ");
+        descuento = ingresoDato.nextInt();
+        Double precioFinal = precioOriginal - (precioOriginal * descuento / 100);
+        System.out.println("El precio final es: " + precioFinal);
+    }
+}
