c3d65a88a41a98abeafe9cc3d48af85b520fb0e7
FedericoLazarte <fedelazarte2015@gmail.com>
2023-06-06 19:13:11 -0300

    subiendo quinto ejercicio

---
+++ clase5/EjercicioCinco.java
@@ -0,0 +1,16 @@
+package clase5;
+import java.util.*;
+public class EjercicioCinco {
+    public static void main(String[] args) {
+        Integer numeroRandom = (int) ((Math.random() * 100) + 1);
+        Integer numero;
+        Scanner ingresoDatos = new Scanner(System.in);
+        System.out.println("Ingrese número a adivinar");
+        numero = ingresoDatos.nextInt();
+        if (numeroRandom == numero) {
+            System.out.println("Adivino!!, el número era: " + numeroRandom);
+        } else {
+            System.out.println("Fallo!!, el número es: " + numeroRandom);
+        }
+    }
+}
