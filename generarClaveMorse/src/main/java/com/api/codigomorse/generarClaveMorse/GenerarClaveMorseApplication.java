5ed857aa9816c0070f1deade4f37f8fd9324b586
federico lazarte <97749958+FedericoLazarte@users.noreply.github.com>
2023-06-16 18:47:45 -0300

    Add files via upload

---
+++ generarClaveMorse/src/main/java/com/api/codigomorse/generarClaveMorse/GenerarClaveMorseApplication.java
@@ -0,0 +1,13 @@
+package com.api.codigomorse.generarClaveMorse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerarClaveMorseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerarClaveMorseApplication.class, args);
+	}
+
+}
