7b24301c57a2e133432b49ffac3117420866bd36
federico lazarte <97749958+FedericoLazarte@users.noreply.github.com>
2023-07-23 16:53:10 -0300

    Add files via upload

---
+++ apis/busquedaPorNombre/src/test/java/com/busqueda/busquedaPorNombre/BusquedaPorNombreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busqueda.busquedaPorNombre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusquedaPorNombreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
