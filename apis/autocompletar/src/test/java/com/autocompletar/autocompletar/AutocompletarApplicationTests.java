79c8c181cf08b30f22e599a63b2821f97ad4d520
federico lazarte <97749958+FedericoLazarte@users.noreply.github.com>
2023-07-23 16:51:48 -0300

    Add files via upload

---
+++ apis/autocompletar/src/test/java/com/autocompletar/autocompletar/AutocompletarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autocompletar.autocompletar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocompletarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
